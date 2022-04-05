1eba8935ec24ba4fdba737ad6d4ee981d24fbc20
dhp2616 <dhp2616@gmail.com>
2022-04-05 19:44:33 -0400

    added names for everyone

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,15 @@ function App(): JSX.Element {
         <div className="App">
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
+                <ul>
+                    <li>Adam O</li>
+                    <li>Rehan C</li>
+                    <li>Dhruv P</li>
+                </ul>
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.a
+                automatically reload.
             </p>
         </div>
     );
