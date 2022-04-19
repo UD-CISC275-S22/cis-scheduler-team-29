13f913d10df44df88bd0ebc135827ce6b349a653
Adam oberembt <adamoberembt@adams-mbp-4.myfiosgateway.com>
2022-04-19 15:29:31 -0400

    fixed course list

--- src/Components/AddCourseTest.tsx
+++ src/Components/AddCourseTest.tsx
@@ -4,21 +4,28 @@ import Autocomplete from "@mui/material/Autocomplete";
 import { useState } from "react";
 import { Courses } from "../Interfaces/Courses";
 import COURSES2 from "../Data/CISC_Courses.json";
+import { ListCourses } from "./ListCourses";
 
 export function AddCourse(): JSX.Element {
-    const temp = COURSES2.map((name: Courses): string => name.CourseName);
+    const temp = COURSES2.map((name: Courses): string => name.Code);
+    const temp2 = COURSES2.map((name: Courses): Courses => name);
 
     const [courses, setCourses] = useState<string[]>([]);
     const [inputValue, setInputValue] = React.useState("");
-    const [value, setValue] = React.useState<string | null>(temp[0]);
+    const [value, setValue] = React.useState<string | null>(temp2[0].Code);
 
     function addCourse(name: string) {
-        if (!courses.includes(name)) {
+        if (!courses.includes(name) && name !== "") {
             const newCourses = [...courses, inputValue];
             setCourses(newCourses);
         } else {
             const newCourses = [...courses];
             setCourses(newCourses);
+        }
+    }
+    function filterByName(obj: Courses) {
+        if (courses.includes(obj.Code)) {
+            return true;
         }
     }
     return (
@@ -50,11 +57,14 @@ export function AddCourse(): JSX.Element {
                     Add Course
                 </button>
             </div>
-            <ul>
+            <ListCourses course={temp2.filter(filterByName)}></ListCourses>
+        </div>
+    );
+}
+/*
+<ul>
                 {courses.map((person: string) => (
                     <li key={person}>{person}</li>
                 ))}
             </ul>
-        </div>
-    );
-}
+*/
