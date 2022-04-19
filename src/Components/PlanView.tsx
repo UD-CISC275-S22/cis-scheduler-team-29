ba4d0a9ff15c84e75ecb9fa437c944bc6e44fb92
Adam oberembt <adamoberembt@wifi-roaming-128-4-131-84.wifi.udel.edu>
2022-04-19 14:25:51 -0400

    now finished

--- src/Components/PlanView.tsx
+++ src/Components/PlanView.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Container, Row, Col, Button } from "react-bootstrap";
 import { Plan } from "../Interfaces/Plan";
-import { AddCourse } from "./AddCourseTest";
 import { SemesterModal } from "./SemesterModal";
 
 export function PlanView({
