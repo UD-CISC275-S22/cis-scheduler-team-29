b5662635b34bd1f3c3ebc7c9cb37049667917a91
Adam oberembt <adamoberembt@wifi-roaming-128-4-113-114.wifi.udel.edu>
2022-04-25 09:15:12 -0400

    sorted the semesters

--- src/Components/SemesterList.tsx
+++ src/Components/SemesterList.tsx
@@ -11,23 +11,34 @@ export function SemesterList({
     deleteSemester: (id: string) => void;
 }): JSX.Element {
     const [hidden, setHidden] = useState(false);
+    function sortBySeason(semester: Semester[]) {
+        const seasonName: string[] = ["Summer", "Fall", "Winter", "Spring"];
+        semester.sort(function (a, b) {
+            return seasonName.indexOf(a.season) - seasonName.indexOf(b.season);
+        });
+        return semester;
+    }
     return (
         <div>
             <button onClick={() => setHidden(!hidden)}>HIDE SEMESTERS</button>
             <div hidden={hidden}>
                 <Stack className="SemesterList" direction="horizontal" gap={3}>
-                    {semesters.map((semester: Semester) => (
-                        <div
-                            key={semester.id}
-                            // className="bg-light border m-2 p-2"
-                            className="hey"
-                        >
-                            <SemesterView
-                                semester={semester}
-                                deleteSemester={deleteSemester}
-                            ></SemesterView>
-                        </div>
-                    ))}
+                    {sortBySeason(semesters)
+                        .sort(function (a, b) {
+                            return a.year.localeCompare(b.year);
+                        })
+                        .map((semester: Semester) => (
+                            <div
+                                key={semester.id}
+                                // className="bg-light border m-2 p-2"
+                                className="hey"
+                            >
+                                <SemesterView
+                                    semester={semester}
+                                    deleteSemester={deleteSemester}
+                                ></SemesterView>
+                            </div>
+                        ))}
                 </Stack>
             </div>
         </div>
