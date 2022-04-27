d70c8438ef22494678d1d345ee42c89ab14f9a79
Adam oberembt <adamoberembt@wifi-roaming-128-4-117-161.wifi.udel.edu>
2022-04-27 16:03:29 -0400

    commit

--- src/Components/SemesterList.tsx
+++ src/Components/SemesterList.tsx
@@ -1,6 +1,5 @@
 import { Grid } from "@mui/material";
-import React, { useState } from "react";
-import { Button } from "react-bootstrap";
+import React from "react";
 import { Semester } from "../Interfaces/Semester";
 import { SemesterView } from "./SemesterView";
 
@@ -11,8 +10,6 @@ export function SemesterList({
     semesters: Semester[];
     deleteSemester: (id: string) => void;
 }): JSX.Element {
-    const [hidden, setHidden] = useState(false);
-
     function sortBySeason(semester: Semester[]) {
         const seasonName: string[] = ["Summer", "Fall", "Winter", "Spring"];
         semester.sort(function (a, b) {
@@ -23,29 +20,24 @@ export function SemesterList({
 
     return (
         <div>
-            <Button onClick={() => setHidden(!hidden)}>
-                {hidden ? <div>SHOW SEMESTERS</div> : <div>HIDE SEMESTERS</div>}
-            </Button>
-            <div hidden={hidden}>
-                <Grid className="SemesterList" direction="row" gap={3}>
-                    {sortBySeason(semesters)
-                        .sort(function (a, b) {
-                            return a.year.localeCompare(b.year);
-                        })
-                        .map((semester: Semester) => (
-                            <div
-                                key={semester.id}
-                                // className="bg-light border m-2 p-2"
-                                className="hey"
-                            >
-                                <SemesterView
-                                    semester={semester}
-                                    deleteSemester={deleteSemester}
-                                ></SemesterView>
-                            </div>
-                        ))}
-                </Grid>
-            </div>
+            <Grid className="SemesterList" direction="row" gap={3}>
+                {sortBySeason(semesters)
+                    .sort(function (a, b) {
+                        return a.year.localeCompare(b.year);
+                    })
+                    .map((semester: Semester) => (
+                        <div
+                            key={semester.id}
+                            // className="bg-light border m-2 p-2"
+                            className="hey"
+                        >
+                            <SemesterView
+                                semester={semester}
+                                deleteSemester={deleteSemester}
+                            ></SemesterView>
+                        </div>
+                    ))}
+            </Grid>
         </div>
     );
 }
